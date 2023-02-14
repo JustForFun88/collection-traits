f34891da80e0da533a9aef1eb0c7c64de9aa120a
JustForFun88 <alishergaliev88@gmail.com>
2023-02-14 09:29:58 +0500

    Doc VecCollectionRef

--- src/vec/vec_ref.rs
+++ src/vec/vec_ref.rs
@@ -2,13 +2,88 @@ use crate::{Equivalent, ValueCollectionRef, ValueContain};
 use smallvec::SmallVec;
 use std::collections;
 
+/// A trait that allows abstraction from different types of sequence
+/// collections.
+///
+/// Provides an API that allows you to work with both the owning or
+/// reference variable.
+///
+/// # Note
+///
+/// When implementing this trait, you need to be careful about the type `E`
+/// of the  lookup value. For greater flexibility, there are no restrictions
+/// on the type of search value, but one of two conditions must be met:
+///
+/// 1. If it is possible to implement it, then it is desirable to specify
+///    the condition `E: Equivalent<Self::Value>`.
+/// 2. If the first condition cannot be met (e.g. for [`std::collections::HashSet`]),
+///    the value **must be** any borrowed form of the container's value type (i.e.
+///    `Self::Value: Borrow<E>` ) .
+///
+/// Note that a container that implements `E: Equivalent<Self::Value>` will also
+/// accept all `E` lookup values such as `Self::Value: Borrow<E>`, but the reverse
+/// is not true.
 pub trait VecCollectionRef<E = <Self as ValueContain>::Value>
 where
     Self: ValueCollectionRef<E> + Sized,
     E: ?Sized,
 {
+    /// Returns a reference to the value at the `index` in
+    /// the vector, or `None` if the `index` is out of bounds.
+    ///
+    /// The implementation of the method depends on the container type,
+    /// so it can take as `O(1)` so as `O(log n)` time.
+    ///
+    /// Time: O(log n)
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use collection_traits::VecCollectionRef;
+    /// let vec = vec!["Joe", "Mike", "Robert"];
+    /// // Unfortunately, when calling the `ValueCollectionRef` methods directly,
+    /// // you need to specify the type `E` because the compiler can't infer it.
+    /// assert_eq!(
+    ///     Some(&"Robert"),
+    ///     VecCollectionRef::<str>::get_value_at(&vec, 2)
+    /// );
+    ///
+    /// // But you do not need to specify the type E when using VecCollectionRef
+    /// // as trait bound
+    /// fn get_at<T>(collection: &T, index: usize) -> Option<&T::Value>
+    /// where
+    ///     T: VecCollectionRef,
+    /// {
+    ///     collection.get_value_at(index)
+    /// }
+    /// assert_eq!(Some(&"Robert"), get_at(&vec, 2));
+    /// assert_eq!(None, get_at(&vec, 5));
+    /// ```
     fn get_value_at(&self, index: usize) -> Option<&Self::Value>;
     fn collection_as_slice(&self) -> Option<&[Self::Value]>;
+}
+
+#[test]
+fn test() {
+    use crate::VecCollectionRef;
+    let vec = vec!["Joe", "Mike", "Robert"];
+    // Unfortunately, when calling the `ValueCollectionRef` methods directly,
+    // you need to specify the type `E` because the compiler can't infer it.
+    assert_eq!(
+        Some(&"Robert"),
+        VecCollectionRef::<str>::get_value_at(&vec, 2)
+    );
+
+    // But you do not need to specify the type E when using VecCollectionRef
+    // as trait bound
+    fn get_at<T>(collection: &T, index: usize) -> Option<&T::Value>
+    where
+        T: VecCollectionRef,
+    {
+        collection.get_value_at(index)
+    }
+    assert_eq!(Some(&"Robert"), get_at(&vec, 2));
+    assert_eq!(None, get_at(&vec, 5));
 }
 
 impl<T, E: ?Sized> VecCollectionRef<E> for &T
